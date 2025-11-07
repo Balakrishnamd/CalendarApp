bf0fb1cddae9961b6b983e9fffc36fb317c8d7da
Balakrishnamd <balakrishna.md1234@gmail.com>
2025-11-07 21:00:14 +0530

    Clean repo and ignore node_modules

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/CalendarApp/',
   plugins: [react()],
-  // If you deploy to GitHub Pages under a repo, set base='/your-repo-name/'
-  // base: '/react-calendar-pro/',
+  
 })
